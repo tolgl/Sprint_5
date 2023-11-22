62b083b9029285280eebf10284a84de5f1d91de7
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-11-22 17:43:33 +0300

    добавлено заполнение полей email и пароль через фикстуры

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,127 +4,117 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-class Login:
+def test_login_by_button_in_main_successful_login(get_email, get_password):
 
-    def __init__(self):
-        self.email = 'testglinkin1997123@ya.ru'
-        self.password = '123456'
+    driver = webdriver.Chrome()
+    driver.get('https://stellarburgers.nomoreparties.site/')
 
-    def test_login_by_button_in_main_successful_login(self):
+    # Кнопка "Войти в аккаунт"
+    driver.find_element(By.XPATH, ".//button[text() = 'Войти в аккаунт']").click()
+    # Поле "Email"
+    driver.find_element(By.XPATH, ".//fieldset[1]//input").send_keys(get_email)
+    # Поле "Пароль"
+    driver.find_element(By.XPATH, ".//fieldset[2]//input").send_keys(get_password)
+    # Кнопка "Войти"
+    driver.find_element(By.XPATH, ".//button[text() = 'Войти']").click()
 
-        driver = webdriver.Chrome()
-        driver.get('https://stellarburgers.nomoreparties.site/')
+    # Ожидаем появления кнопки "Оформить заказ" на главной странице
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, ".//button[text() = 'Оформить заказ']")))
 
-        # Кнопка "Войти в аккаунт"
-        driver.find_element(By.XPATH, ".//button[text() = 'Войти в аккаунт']").click()
-        # Поле "Email"
-        driver.find_element(By.XPATH, ".//fieldset[1]//input").send_keys(self.email)
-        # Поле "Пароль"
-        driver.find_element(By.XPATH, ".//fieldset[2]//input").send_keys(self.password)
-        # Кнопка "Войти"
-        driver.find_element(By.XPATH, ".//button[text() = 'Войти']").click()
+    # Получаем текст кнопки "Оформить заказ"
+    element = driver.find_element(By.XPATH, ".//button[text() = 'Оформить заказ']").text
 
-        # Ожидаем появления кнопки "Оформить заказ" на главной странице
-        WebDriverWait(driver, 3).until(
-            expected_conditions.visibility_of_element_located((By.XPATH, ".//button[text() = 'Оформить заказ']")))
+    # проверяем что появилась кнопка "Оформить заказ" на главной странице
+    assert element == 'Оформить заказ'
 
-        # Получаем текст кнопки "Оформить заказ"
-        element = driver.find_element(By.XPATH, ".//button[text() = 'Оформить заказ']").text
-
-        # проверяем что появилась кнопка "Оформить заказ" на главной странице
-        assert element == 'Оформить заказ'
-
-        driver.quit()
-
-    def test_login_by_link_in_header_successful_login(self):
-
-        driver = webdriver.Chrome()
-        driver.get('https://stellarburgers.nomoreparties.site/')
-
-        # Ссылка "Личный кабинет"
-        driver.find_element(By.XPATH, ".//a[@href='/account']").click()
-        # Поле "Email"
-        driver.find_element(By.XPATH, ".//fieldset[1]//input").send_keys(self.email)
-        # Поле "Пароль"
-        driver.find_element(By.XPATH, ".//fieldset[2]//input").send_keys(self.password)
-        # Кнопка "Войти"
-        driver.find_element(By.XPATH, ".//button[text() = 'Войти']").click()
-
-        # Ожидаем появления кнопки "Оформить заказ" на главной странице
-        WebDriverWait(driver, 3).until(
-            expected_conditions.visibility_of_element_located((By.XPATH, ".//button[text() = 'Оформить заказ']")))
-
-        # Получаем текст кнопки "Оформить заказ"
-        element = driver.find_element(By.XPATH, ".//button[text() = 'Оформить заказ']").text
-
-        # проверяем что появилась кнопка "Оформить заказ" на главной странице
-        assert element == 'Оформить заказ'
-
-        driver.quit()
-
-    def test_login_by_link_on_form_registration_successful_login(self):
-
-        driver = webdriver.Chrome()
-        driver.get('https://stellarburgers.nomoreparties.site/')
-
-        # Ссылка "Личный кабинет"
-        driver.find_element(By.XPATH, ".//a[@href='/account']").click()
-        # Ссылка "Зарегистрироваться"
-        driver.find_element(By.LINK_TEXT, "Зарегистрироваться").click()
-        # Ссылка "Войти"
-        driver.find_element(By.LINK_TEXT, "Войти").click()
-        # Поле "Email"
-        driver.find_element(By.XPATH, ".//fieldset[1]//input").send_keys(self.email)
-        # Поле "Пароль"
-        driver.find_element(By.XPATH, ".//fieldset[2]//input").send_keys(self.password)
-        # Кнопка "Войти"
-        driver.find_element(By.XPATH, ".//button[text() = 'Войти']").click()
-
-        # Ожидаем появления кнопки "Оформить заказ" на главной странице
-        WebDriverWait(driver, 3).until(
-            expected_conditions.visibility_of_element_located((By.XPATH, ".//button[text() = 'Оформить заказ']")))
-
-        # Получаем текст кнопки "Оформить заказ"
-        element = driver.find_element(By.XPATH, ".//button[text() = 'Оформить заказ']").text
-
-        # проверяем что появилась кнопка "Оформить заказ" на главной странице
-        assert element == 'Оформить заказ'
-
-        driver.quit()
-
-    def test_login_by_link_on_form_password_recovery_successful_login(self):
-
-        driver = webdriver.Chrome()
-        driver.get('https://stellarburgers.nomoreparties.site/')
-
-        # Ссылка "Личный кабинет"
-        driver.find_element(By.XPATH, ".//a[@href='/account']").click()
-        # Ссылка "Восстановить пароль"
-        driver.find_element(By.LINK_TEXT, "Восстановить пароль").click()
-        # Ссылка "Войти"
-        driver.find_element(By.LINK_TEXT, "Войти").click()
-        # Поле "Email"
-        driver.find_element(By.XPATH, ".//fieldset[1]//input").send_keys(self.email)
-        # Поле "Пароль"
-        driver.find_element(By.XPATH, ".//fieldset[2]//input").send_keys(self.password)
-        # Кнопка "Войти"
-        driver.find_element(By.XPATH, ".//button[text() = 'Войти']").click()
-
-        # Ожидаем появления кнопки "Оформить заказ" на главной странице
-        WebDriverWait(driver, 3).until(
-            expected_conditions.visibility_of_element_located((By.XPATH, ".//button[text() = 'Оформить заказ']")))
-
-        # Получаем текст кнопки "Оформить заказ"
-        element = driver.find_element(By.XPATH, ".//button[text() = 'Оформить заказ']").text
-
-        # проверяем что появилась кнопка "Оформить заказ" на главной странице
-        assert element == 'Оформить заказ'
-
-        driver.quit()
+    driver.quit()
 
 
-login = Login()
-login.test_login_by_button_in_main_successful_login()
-login.test_login_by_link_in_header_successful_login()
-login.test_login_by_link_on_form_registration_successful_login()
-login.test_login_by_link_on_form_password_recovery_successful_login()
+def test_login_by_link_in_header_successful_login(get_email, get_password):
+
+    driver = webdriver.Chrome()
+    driver.get('https://stellarburgers.nomoreparties.site/')
+
+    # Ссылка "Личный кабинет"
+    driver.find_element(By.XPATH, ".//a[@href='/account']").click()
+    # Поле "Email"
+    driver.find_element(By.XPATH, ".//fieldset[1]//input").send_keys(get_email)
+    # Поле "Пароль"
+    driver.find_element(By.XPATH, ".//fieldset[2]//input").send_keys(get_password)
+    # Кнопка "Войти"
+    driver.find_element(By.XPATH, ".//button[text() = 'Войти']").click()
+
+    # Ожидаем появления кнопки "Оформить заказ" на главной странице
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, ".//button[text() = 'Оформить заказ']")))
+
+    # Получаем текст кнопки "Оформить заказ"
+    element = driver.find_element(By.XPATH, ".//button[text() = 'Оформить заказ']").text
+
+    # проверяем что появилась кнопка "Оформить заказ" на главной странице
+    assert element == 'Оформить заказ'
+
+    driver.quit()
+
+
+def test_login_by_link_on_form_registration_successful_login(get_email, get_password):
+
+    driver = webdriver.Chrome()
+    driver.get('https://stellarburgers.nomoreparties.site/')
+
+    # Ссылка "Личный кабинет"
+    driver.find_element(By.XPATH, ".//a[@href='/account']").click()
+    # Ссылка "Зарегистрироваться"
+    driver.find_element(By.LINK_TEXT, "Зарегистрироваться").click()
+    # Ссылка "Войти"
+    driver.find_element(By.LINK_TEXT, "Войти").click()
+    # Поле "Email"
+    driver.find_element(By.XPATH, ".//fieldset[1]//input").send_keys(get_email)
+    # Поле "Пароль"
+    driver.find_element(By.XPATH, ".//fieldset[2]//input").send_keys(get_password)
+    # Кнопка "Войти"
+    driver.find_element(By.XPATH, ".//button[text() = 'Войти']").click()
+
+    # Ожидаем появления кнопки "Оформить заказ" на главной странице
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, ".//button[text() = 'Оформить заказ']")))
+
+    # Получаем текст кнопки "Оформить заказ"
+    element = driver.find_element(By.XPATH, ".//button[text() = 'Оформить заказ']").text
+
+    # проверяем что появилась кнопка "Оформить заказ" на главной странице
+    assert element == 'Оформить заказ'
+
+    driver.quit()
+
+
+def test_login_by_link_on_form_password_recovery_successful_login(get_email, get_password):
+
+    driver = webdriver.Chrome()
+    driver.get('https://stellarburgers.nomoreparties.site/')
+
+    # Ссылка "Личный кабинет"
+    driver.find_element(By.XPATH, ".//a[@href='/account']").click()
+    # Ссылка "Восстановить пароль"
+    driver.find_element(By.LINK_TEXT, "Восстановить пароль").click()
+    # Ссылка "Войти"
+    driver.find_element(By.LINK_TEXT, "Войти").click()
+    # Поле "Email"
+    driver.find_element(By.XPATH, ".//fieldset[1]//input").send_keys(get_email)
+    # Поле "Пароль"
+    driver.find_element(By.XPATH, ".//fieldset[2]//input").send_keys(get_password)
+    # Кнопка "Войти"
+    driver.find_element(By.XPATH, ".//button[text() = 'Войти']").click()
+
+    # Ожидаем появления кнопки "Оформить заказ" на главной странице
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, ".//button[text() = 'Оформить заказ']")))
+
+    # Получаем текст кнопки "Оформить заказ"
+    element = driver.find_element(By.XPATH, ".//button[text() = 'Оформить заказ']").text
+
+    # проверяем что появилась кнопка "Оформить заказ" на главной странице
+    assert element == 'Оформить заказ'
+
+    driver.quit()
