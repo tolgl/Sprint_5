bfd8a160c7f08341aa25f2320028e3257a3a1fe2
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-11-22 17:54:04 +0300

    удален импорт pytest

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-import pytest
-
 
 def test_correct_data_successful_registration(name_generation, email_generation, password_generation):
     driver = webdriver.Chrome()
