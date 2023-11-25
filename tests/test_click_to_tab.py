8d8dc91de13ac4988b78d8c1d9e7b940f74b075a
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-11-25 21:54:34 +0300

    оптимизированы локаторы поиска табов

--- tests/test_click_to_tab.py
+++ tests/test_click_to_tab.py
@@ -3,11 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-import pytest
-
 
 def test_successful_switching_on_tab_stuffing():
-
     driver = webdriver.Chrome()
     driver.get('https://stellarburgers.nomoreparties.site/')
 
@@ -17,9 +14,9 @@ def test_successful_switching_on_tab_stuffing():
             (By.CLASS_NAME, "BurgerIngredients_ingredients__menuContainer__Xu3Mo")))
 
     # Таб "Начинки"
-    driver.find_element(By.XPATH, ".//div/main/section[1]/div[1]/div[3]").click()
+    driver.find_element(By.XPATH, ".//span[text() = 'Начинки']").click()
     # получение атрибута таба "Начинки"
-    attribute = driver.find_element(By.XPATH, ".//div/main/section[1]/div[1]/div[3]").get_attribute('class')
+    attribute = driver.find_element(By.XPATH, ".//span[text() = 'Начинки']//parent::div").get_attribute('class')
 
     # проверяем что значение атрибута при активном табе есть в нажатом табе
     assert 'tab_tab_type_current__2BEPc' in attribute
@@ -38,9 +35,9 @@ def test_successful_switching_on_tab_sauce():
             (By.CLASS_NAME, "BurgerIngredients_ingredients__menuContainer__Xu3Mo")))
 
     # Таб "Соусы"
-    driver.find_element(By.XPATH, ".//div/main/section[1]/div[1]/div[2]").click()
+    driver.find_element(By.XPATH, ".//span[text() = 'Соусы']").click()
     # получение атрибута таба "Соусы"
-    attribute = driver.find_element(By.XPATH, ".//div/main/section[1]/div[1]/div[2]").get_attribute('class')
+    attribute = driver.find_element(By.XPATH, ".//span[text() = 'Соусы']//parent::div").get_attribute('class')
 
     # проверяем что значение атрибута при активном табе есть в нажатом табе
     assert 'tab_tab_type_current__2BEPc' in attribute
@@ -59,7 +56,7 @@ def test_successful_switching_on_tab_bread():
             (By.CLASS_NAME, "BurgerIngredients_ingredients__menuContainer__Xu3Mo")))
 
     # получение атрибута таба "Булки"
-    attribute = driver.find_element(By.XPATH, ".//div/main/section[1]/div[1]/div[1]").get_attribute('class')
+    attribute = driver.find_element(By.XPATH, ".//span[text() = 'Булки']//parent::div").get_attribute('class')
 
     # проверяем что значение атрибута при активном табе есть в нажатом табе
     assert 'tab_tab_type_current__2BEPc' in attribute
